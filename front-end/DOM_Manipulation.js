4b6b3d1347581884ed71ba7d003a3ea6d5bb9150
william20047 <a.william.20047@gmail.com>
2023-08-16 08:00:36 -0400

    added to constructor and made new board page

--- front-end/DOM_Manipulation.js
+++ front-end/DOM_Manipulation.js
@@ -3,11 +3,31 @@ const mainDiv = document.querySelector('#mainContainer')
 class Page{
     
     constructor(content){
-        this.element = content
+        console.log(this)
+
+        this.element = document.createElement('div')
+        this.element.id = 'pageContainer'
+
+        this.element.innerHTML = content
+
+        function processElement(self, element) {
+            
+            if(element.id != ''){
+                self[element.id + 'Element'] = element
+            }
+        
+            // Iterate over its children
+            Array.from(element.children).forEach(child => {
+                processElement(self, child);
+            });
+        }
+
+        processElement(this, this.element)
     }
 
     changeTo = () => {
-        mainDiv.innerHTML = this.element
+        mainDiv.innerHTML = ''
+        mainDiv.append(this.element)
     }
 
 
@@ -17,13 +37,40 @@ let main = new Page(
 `<h1>Tic Tac Toe</h1>
 <nav>
     <ul class="menuButtons">
-        <li><button class="menuButton" id="singlePlayer">Single Player</button></li>
+        <li><button class="menuButton" id="singleplayer">Singleplayer</button></li>
         <li><button class="menuButton comingSoon" id="multiPlayer">Multiplayer</button></li>
         <li><button class="menuButton comingSoon" id="settings">Settings</button></li>
     </ul>
-</nav>`,)
+</nav>`)
 
 
 
 
-main.changeTo()
+let board = new Page(
+`<div id="gameBoard">
+    <div id="board-line-row-1" class="board-line board-line-row"></div>
+    <div id="board-line-row-2" class="board-line board-line-row"></div>
+    <div id="board-line-column-1" class="board-line  board-line-column"></div>
+    <div id="board-line-column-2" class="board-line  board-line-column"></div>
+
+    <div x="0" y="0" class="board-slot"></div>
+    <div x="1" y="0" class="board-slot"></div>
+    <div x="2" y="0" class="board-slot"></div>
+    <div x="0" y="1" class="board-slot"></div>
+    <div x="1" y="1" class="board-slot"></div>
+    <div x="2" y="1" class="board-slot"></div>
+    <div x="0" y="2" class="board-slot"></div>
+    <div x="1" y="2" class="board-slot"></div>
+    <div x="2" y="2" class="board-slot"></div>
+</div>`)
+
+
+
+// main.changeTo()
+
+main.singleplayerElement.addEventListener('click', ()=>{
+    board.changeTo()
+})
+
+
+board.changeTo()
